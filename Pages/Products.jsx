1c5473dec5ed01dde96b9e5d7617caf0debd2d39
Prakash Dsouza <84118928+PRAKASH-1971@users.noreply.github.com>
2022-06-21 22:57:58 +0530

    Add files via upload

---
+++ Pages/Products.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {Box} from "@chakra-ui/react"
+import FilterComponent from '../Components/FilterComponent'
+
+const Products = () => {
+  return (
+    <div>
+        <Box><FilterComponent/></Box>
+        <Box>
+            {/* <Products /> */}
+        </Box>
+    </div>
+  )
+}
+
+export default Products
---
+++ index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import {BrowserRouter} from "react-router-dom"
+import {ChakraProvider, ColorModeScript} from "@chakra-ui/react"
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <BrowserRouter>
+  <ChakraProvider>
+  <ColorModeScript initialColorMode="light"></ColorModeScript>
+<App />
+</ChakraProvider>
+</BrowserRouter>
+);
+
